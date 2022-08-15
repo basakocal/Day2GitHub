eb98717c7e863a63fa376b95879605dbab5b8aad
Basak Ocal <basaktarik@gmail.com>
2022-08-15 23:00:08 +0300

    V4

--- src/Day2GitGitHub.java
+++ src/Day2GitGitHub.java
@@ -24,6 +24,7 @@ public class Day2GitGitHub {
      */
     public static void main(String[] args) {
         System.out.println("Branch 1. deneme");
+        System.out.println("Master 1. deneme");
     }
 
 }
